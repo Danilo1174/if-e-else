b5d2424693d3d6a943d546ca6be2d4ea854c9441
Danilo Alves <daniloalves.1996@gmail.com>
2022-12-09 16:14:47 -0300

    alteracao-if/else

--- Program.cs
+++ Program.cs
@@ -4,120 +4,139 @@ namespace atividade_if_else
 {
     class Program
     {
+        private static int escolha;
+
         static void Main(string[] args)
+
+
         {
-            #region exercicio 1
 
-            
-
-            //int a, b;
-
-            //Console.WriteLine ("digite o valor de a: ");
-            //a = int.Parse(Console.ReadLine  ());
-
-            //Console.WriteLine("digite o valor de b : ");
-            //b = int.Parse(Console.ReadLine  ());
-
-            //if (a % b == 0 || b % a == 0)
-            //{
-            //  Console.WriteLine ("são multiplos");
-            //}
-            //else
-            //{
-            //  Console.WriteLine("não são multiplos");
-            //}
+            Console.WriteLine("escolha um exercicio a ser executado");
+            escolha = int.Parse(Console.ReadLine());
+            switch (escolha != 0)
+            {
+             case 1:
+                        #region exercicio 1
 
 
 
-            #endregion
+                int a, b;
 
-            #region exercicio 2
-            // Exercicio 2
+                Console.WriteLine("digite o valor de a: ");
+                a = int.Parse(Console.ReadLine());
 
-            //int codigo, quantidade;
-            // double preco = 0.0;
+                Console.WriteLine("digite o valor de b : ");
+                b = int.Parse(Console.ReadLine());
 
-            // Console.WriteLine("codigo   | especificacao   |   preco ");
-            // Console.WriteLine("  1      | cachorro quente |   r$4.00");
-            // Console.WriteLine("  2      | x-salada        |   r$4.50");
-            // Console.WriteLine("  3      | x-bacon         |   r$5.00");
-            // Console.WriteLine("  4      | bauru           |   r$2.50");
-            // Console.WriteLine("  5      | refrigerante    |   r$1.50");
-
-
-
-            //  Console.WriteLine("digite o código: ");
-            // codigo = int.Parse(Console.ReadLine());
-
-            // Console.WriteLine("digite a quantidade: ");
-            //  quantidade = int.Parse(Console.ReadLine());
-
-            // if (codigo == 1)
-            // {
-            //     preco = 4.00 * quantidade;
-            // }
-            // else if (codigo == 2)
-            // {
-            //     preco = 4.50 * quantidade;
-            //  }
-            // else if (codigo == 3)
-            // {
-            //     preco = 5.00 * quantidade;
-            //  }
-            // else if (codigo == 4)
-            //  {
-            //     preco = 2.00 * quantidade;
-            //  }
-            // else if (codigo == 5)
-            //  {
-            //     preco = 1.50 * quantidade;
-            // }
-
-            // Console.WriteLine("total: R${0:f2}", preco);
-
-            #endregion
-
-            #region exercicio 3
-
-            // Exercicio 3
+                if (a % b == 0 || b % a == 0)
+                {
+                    Console.WriteLine("são multiplos");
+                }
+                else
+                {
+                    Console.WriteLine("não são multiplos");
+                }
 
 
-            // Console.WriteLine("digite o valor de x: ");
-            //double x = double.Parse(Console.ReadLine());
 
-            // Console.WriteLine ("digite o valor de y:");
-            //double y = double.Parse(Console.ReadLine());
+                #endregion
+                break;
+                
+             case 2:
+
+                        #region exercicio 2
 
 
-            //  if (x == 0.0 && y == 0.0)
-            //{
-            //  Console.WriteLine("origem");
-            //}
-            //else if (x == 0.0)
-            //{
-            //  Console.WriteLine("eixo y");
-            //}
-            //else if (y == 0.0)
-            //{
-            //  Console.WriteLine("eixo x");
-            //}
-            //else if (x > 0.0 && y > 0.0)
-            //{
-            //  Console.WriteLine("q1");
-            //}
-            //else if (x < 0.0 && y > 0.0)
-            //{
-            //  Console.WriteLine("q2");
-            //}
-            // else if (x < 0.0 && y < 0.0)
-            //{
-            //  Console.WriteLine("q3");
-            //}
-            // else
-            //{
-            //  Console.WriteLine("q4");
-            //}
-            #endregion
+                int codigo, quantidade;
+                double preco = 0.0;
+
+                Console.WriteLine("codigo   | especificacao   |   preco ");
+                Console.WriteLine("  1      | cachorro quente |   r$4.00");
+                Console.WriteLine("  2      | x-salada        |   r$4.50");
+                Console.WriteLine("  3      | x-bacon         |   r$5.00");
+                Console.WriteLine("  4      | bauru           |   r$2.50");
+                Console.WriteLine("  5      | refrigerante    |   r$1.50");
+
+
+
+                Console.WriteLine("digite o código: ");
+                codigo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("digite a quantidade: ");
+                quantidade = int.Parse(Console.ReadLine());
+
+                if (codigo == 1)
+                {
+                    preco = 4.00 * quantidade;
+                }
+                else if (codigo == 2)
+                {
+                    preco = 4.50 * quantidade;
+                }
+                else if (codigo == 3)
+                {
+                    preco = 5.00 * quantidade;
+                }
+                else if (codigo == 4)
+                {
+                    preco = 2.00 * quantidade;
+                }
+                else if (codigo == 5)
+                {
+                    preco = 1.50 * quantidade;
+                }
+
+                Console.WriteLine("total: R${0:f2}", preco);
+
+                #endregion
+
+                break;
+
+             case 3:
+                         #region exercicio 3
+
+
+
+
+                Console.WriteLine("digite o valor de x: ");
+                double x = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("digite o valor de y:");
+                double y = double.Parse(Console.ReadLine());
+
+
+                if (x == 0.0 && y == 0.0)
+                {
+                    Console.WriteLine("origem");
+                }
+                else if (x == 0.0)
+                {
+                    Console.WriteLine("eixo y");
+                }
+                else if (y == 0.0)
+                {
+                    Console.WriteLine("eixo x");
+                }
+                else if (x > 0.0 && y > 0.0)
+                {
+                    Console.WriteLine("q1");
+                }
+                else if (x < 0.0 && y > 0.0)
+                {
+                    Console.WriteLine("q2");
+                }
+                else if (x < 0.0 && y < 0.0)
+                {
+                    Console.WriteLine("q3");
+                }
+                else
+                {
+                    Console.WriteLine("q4");
+                }
+                #endregion
+                break;
+            }
         }
     }
 }
+
